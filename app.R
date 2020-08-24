10939dbac6ad2c1721e9235e389c0c377348187c
Wolfgang Huber <wolfgang.huber@embl.de>
2020-08-24 12:08:20 +0200

    Replaced genefilter::rowttests with an apply on stats::t.test to reduce package dependencies

--- app.R
+++ app.R
@@ -1,5 +1,18 @@
 library("shiny")
 library("ggplot2")
+library("tibble")
+
+#----------------------------------------------------
+sim = function(inp, n = 20, m = 2000) {
+  x = matrix(
+    do.call(inp$dist, list(n = n * m)), 
+    ncol = m, nrow = n)
+  rownames(x) = letters[1 + (seq_len(n)-1) %/% (n/2)]
+  stopifnot(nrow(x) >= 2)
+  for (j in 2:nrow(x))
+    x[j, ] = x[j-1, ] * inp$eps + x[j, ] * (1 - inp$eps)
+  x
+}
 
 #----------------------------------------------------
 ui = fluidPage(
@@ -16,27 +29,17 @@ ui = fluidPage(
   ),
   mainPanel(
     plotOutput("plot"),    
-    HTML("The histogram shows the distribution of 10000 p-values from calling the t-test on a comparison between two groups, each with 10 data points. There is no difference between the statistical distributions of the data between the two groups (in particular, no difference between their means). Thus, the histogram should be flat, corresponding to a uniform distribution of null p-values in [0,1]. You can experiment with different elementary statistical distributions from which the random numbers are drawn, and you can introduce and control the strength of serial correlations between subsequent values. <a href='https://github.com/wolfganghuber/t-test-normality-and-independence'>The source code for this app is here: https://github.com/wolfganghuber/t-test-normality-and-independence</a>."),
+    HTML(paste("The histogram shows the distribution of", formals(sim)$m, "p-values from calling the t-test on a comparison between two groups, each with", formals(sim)$n/2, "data points. There is no difference between the statistical distributions of the data between the two groups (in particular, no difference between their means). Thus, the histogram should be flat, corresponding to a uniform distribution of null p-values in [0,1]. You can experiment with different elementary statistical distributions from which the random numbers are drawn, and you can introduce and control the strength of serial correlations between subsequent values. <a href='https://github.com/wolfganghuber/t-test-normality-and-independence'>The source code for this app is here: https://github.com/wolfganghuber/t-test-normality-and-independence</a>.")),
   )
 ))
 
-#----------------------------------------------------
-sim = function(inp, n = 20, m = 10000) {
-  x = matrix(
-    do.call(inp$dist, list(n = n * m)), 
-    ncol = n, nrow = m)
-  colnames(x) = letters[1 + (seq_len(n)-1) %/% (n/2)]
-  stopifnot(ncol(x) >= 2)
-  for (j in 2:ncol(x))
-    x[, j] = x[, j-1] * inp$eps + x[, j] * (1 - inp$eps)
-  x
-}
 
 server = function(input, output) {
   output$plot = renderPlot({
-    xc = sim(input)
-    rt = genefilter::rowttests(xc, factor(colnames(xc)))
-    ggplot(rt, aes(x = p.value)) + geom_histogram(breaks = seq(0, 1, by = 0.0125)) 
+    x = sim(input)
+    d = tibble(p.value = vapply(seq_len(ncol(x)), function(i) 
+      t.test(x[, i] ~ rownames(x))$p.value, numeric(1)))
+    ggplot(d, aes(x = p.value)) + geom_histogram(breaks = seq(0, 1, by = 0.025)) 
   })
 }
 
